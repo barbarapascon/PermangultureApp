2249c8ea8ab8da4c58ae06145178aec8eee9658c
Barbara Pascon de Azevedo Marques <barbarapascon@hotmail.com>
2020-07-29 18:23:30 -0300

    configuracoes iniciais do app e criacao de homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+public options:Array<any>=[
+  { icon: 'person-add-outline',text: 'Plantas'},
+  { icon: 'phone-portrait-outline',text: 'Problemas'},
+  { icon: 'whallet-outline',text: 'Hortas comunitarias'},
+  { icon: 'help-circle-outline',text: 'Tarefas de hoje'},
+  { icon: 'barcode-outline',text: 'Assistente online'}
+];
+public slidesOptions: any ={slidesPerView:3, freeMode:true}
   constructor() {}
 
 }
