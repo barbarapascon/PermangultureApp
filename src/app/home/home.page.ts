60763c273e19d40915084e9505e37a2367e08851
Barbara Pascon de Azevedo Marques <barbarapascon@hotmail.com>
2020-07-30 17:39:34 -0300

    atualizacoes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {Animation, AnimationController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,14 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('blocks') blocks:any;
 public options:Array<any>=[
   { icon: 'person-add-outline',text: 'Plantas'},
   { icon: 'phone-portrait-outline',text: 'Problemas'},
-  { icon: 'whallet-outline',text: 'Hortas comunitarias'},
+  { icon: 'person-add-outline',text: 'Hortas comunitarias'},
   { icon: 'help-circle-outline',text: 'Tarefas de hoje'},
   { icon: 'barcode-outline',text: 'Assistente online'}
 ];
 public slidesOptions: any ={slidesPerView:3, freeMode:true}
-  constructor() {}
 
+public items:Array<any>=[
+  { icon: 'help-circle-outline',text: 'Me ajude'},
+  { icon: 'person-outline',text: 'Perfil'},
+  { icon: 'cash-outline',text: 'Configurar conta'},
+  { icon: 'card-outline',text: 'Configurar plantas'},
+    { icon: 'phone-portrait-outline', text: 'Configuracao do app' }
+  ];
+  public initialStep: number = 0;
+  private maxTranslate: number;
+  private animation: Animation;
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform
+    ) {
+    this.maxTranslate = this.platform.height() - 200;
+  }
+ngAfterViewInit(){
+  this.createAnimation();
+}
+toogleBlocks(){
+  this.initialStep=this.initialStep === 0 ? this.maxTranslate : 0;
+  this.animation.direction(this.initialStep === 0 ? 'reverse': 'normal').play();
+  
+  this.animation.play();
+  console.log(this.animation);
+}
+createAnimation(){
+  this.animation = this.animationCtrl.create()
+  .addElement(this.blocks.nativeElements)
+  .duration(300)
+  .fromTo('transform', 'translate(0)',`translateY(${this.maxTranslate}px)`);
+console.log(this.animation);
+}
 }
